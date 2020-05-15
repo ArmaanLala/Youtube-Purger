167ae7884fb4e4fbfa6df16e74d990aae0ea92ff
Armaan <armaanlala@gmail.com>
2020-05-14 21:57:25 -0400

    Finished the print subscriptions method

--- yt-purge.py
+++ yt-purge.py
@@ -5,26 +5,50 @@ from creditentials import email, password
 
 class PurgerBot:
 
-    #This method opens up the browser and logs the user in
+    # This method opens up the browser and logs the user in
     def __init__(self, email, password):
         self.driver = webdriver.Chrome()
         self.driver.get("https://youtube.com")
+        self.driver.maximize_window()
         sleep(3)
         self.driver.find_element_by_xpath(
             "/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[3]/div[2]/ytd-button-renderer/a/paper-button").click()
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(email)
         self.driver.find_element_by_xpath(
+            "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span").click()
+        sleep(3)
+        self.driver.find_element_by_xpath(
             "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input").send_keys(password)
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/span/span").click()
 
         # The line below is for two-factor authentication and you will need to do this part on your own
-        sleep(60)
+        sleep(5)
 
         # If you only have one youtube account tied to this email, comment the next line out
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/div[3]/div/div/div/div/div/div[1]/div[2]/div/form/div[2]/button/span").click()
+        sleep(5)
 
-myBot = PurgerBot(email,password)
+    #Prints all your subscriptions in a txt file
+    def listSubscriptions(self):
+        subs = open("subscription.txt", "w")
+        self.driver.find_element_by_xpath(
+            "/html/body/ytd-app/div/app-drawer/div[2]/div/div[2]/div[2]/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[2]/div/ytd-guide-collapsible-entry-renderer/ytd-guide-entry-renderer/a/paper-item").click()
+        subscriptions = self.driver.find_element_by_xpath(
+            "/html/body/ytd-app/div/app-drawer/div[2]/div/div[2]/div[2]/ytd-guide-renderer/div[1]/ytd-guide-section-renderer[2]/div")
+        channels = subscriptions.find_elements_by_id("endpoint")
+        # print(looper)
+        for item in channels:
+            name = item.get_attribute("title")
+            subs.write(name + "\n")
+            # name = item.find_element_by_id("endpoint")
+            # print(name)
 
+    
+
+
+# myBot = PurgerBot(email,password)
+# myBot.listSubscriptions()
+# myBot.listSubscriptions()
